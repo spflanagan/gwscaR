aae7771af8e4193b505ce96fd4df28887061c367
Sarah <spflanagan.phd@gmail.com>
2017-06-21 17:12:17 -0400

    dividing functions into multiple scripts

---
+++ R/gwscaR_plot.R
@@ -0,0 +1,349 @@
+#Author: Sarah P. Flanagan
+#Purpose: Plot genome-wide statistics and perform other population genomics visualizations
+#Package: gwscaR
+
+
+#' Plot genome-wide statistics from a data frame.
+#' @param fst.dat The data.frame containing at least three columns: the statistic to be plotted, the chromosome ID, and the BP ID for each locus. Each row is a locus.
+#' @param pt.cols The color of the points. The default is c("darkgrey","lightgrey").
+#' @param fst.name The name of the column containing the statistic to be plotted. Default is "Fst".
+#' @param chrom.name The name of the column containing the chromosome information for each locus. Default is "Chrom".
+#' @param bp.name The name of the column containing the basepair information for each locus. Default is "BP".
+#' @param axis.size The value of cex.axis passed to the plotting of the y-axis. Default is 0.5. Set axis.size = 0 to suppress plotting of the y-axis.
+#' @param scaffs.to.plot A vector or list containing the names of the chromosomes/scaffolds you want to include in the order in which you would like them to be plotted.
+#' @param y.lim The limits for the y-axis.
+#' @param pt.cex The size of the points
+#' @param xlabels Either a boolean (TRUE) or a vector of labels for the x-axis
+#' @param xlab.indices If the xlabels are not the same as the names of the chromosomes in fst.dat, then provide xlab.indices, which is a vector of integers that are the indices in scaffs.to.plot for which chromosome sets you would like to label.
+#' @param scaffold.widths A data.frame with first column naming the chromosomes/linkage groups, second column having the last number of loci on the chromosome for indexing purposes.
+#' e.g.:
+#'  Chrom   End
+#'  LG1     3945850
+#'  LG2     435215
+#' This parameter is used to make multiple plots from the same overall dataset have the same widths.
+#' @return new.dat The fst.dat data frame with new values in BP, scaled to be the sequence in which points are plotted based on their position on the chromosome and the order the chromosomes are plotted in.
+#' @seealso Flanagan & Jones 2017
+#' @export
+fst.plot<-function(fst.dat,scaffold.widths=NULL,scaffs.to.plot=NULL,
+                   fst.name="Fst", chrom.name="Chrom", bp.name="BP",
+                   y.lim=NULL,xlabels=NULL,xlab.indices=NULL,
+                   axis.size=0.5,pt.cols=c("darkgrey","lightgrey"),pt.cex=0.5,...){
+  #make sure fsts are numeric
+  fst.dat[,fst.name]<-as.numeric(as.character(fst.dat[,fst.name]))
+  #if scaffold widths aren't provided, we add them ourselves
+  if(is.null(scaffold.widths)){
+    scaffold.widths<-data.frame(Chrom=levels(as.factor(fst.dat[,chrom.name])),
+                                Max=tapply(as.numeric(as.character(fst.dat[,bp.name])),
+                                           fst.dat[,chrom.name],max))
+  }
+  colnames(scaffold.widths)<-c("Chrom","Max")
+  #if no specific scaffolds are provided, we plot them all
+  if(is.null(scaffs.to.plot)){
+    scaffs.to.plot<-scaffold.widths[,1]#unique(fst.dat[,chrom.name])
+  }
+  
+  #set up new dataframe
+  new.dat<-data.frame(stringsAsFactors = F)
+  last.max<-0
+  for(i in 1:length(scaffs.to.plot)){
+    #pull out the data for this scaffold
+    chrom.dat<-fst.dat[fst.dat[,chrom.name] %in% scaffs.to.plot[i],]
+    if(nrow(chrom.dat)>0){
+      chrom.dat$plot.pos<-as.numeric(as.character(chrom.dat[,bp.name]))+last.max
+      new.dat<-rbind(new.dat,chrom.dat)
+      #last.max<-max(chrom.dat$plot.pos)+
+      #               as.numeric(scaffold.widths[scaffold.widths[,1] %in% scaffs.to.plot[i],2])
+    }
+    last.max<-last.max+
+      as.numeric(scaffold.widths[scaffs.to.plot[i],2])
+  }
+  #make sure everything is the correct class
+  new.dat$plot.pos<-as.numeric(as.character(new.dat$plot.pos))
+  
+  new.dat[,chrom.name]<-as.factor(as.character(new.dat[,chrom.name]))
+  scaffs.to.plot<-as.factor(as.character(scaffs.to.plot))
+  #determine the axis limits
+  x.max<-max(new.dat$plot.pos,na.rm=T)
+  x.min<-min(new.dat$plot.pos,na.rm=T)
+  if(is.null(y.lim)){
+    y.max<-max(fst.dat[,fst.name])+0.1*max(fst.dat[,fst.name])
+    y.min<-min(fst.dat[,fst.name])-0.1*min(fst.dat[,fst.name])
+    if(min(fst.dat[,fst.name]) < 0) {
+      y.min<-min(fst.dat[,fst.name]) - 0.1*min(fst.dat[,fst.name])
+    } else {
+      y.min<-0
+    }
+    
+    y.lim<-c(y.min,y.max)
+  }
+  
+  #plot
+  colors<-data.frame(lg=as.character(new.dat[,chrom.name]),col=rep(pt.cols[1],nrow(new.dat)),
+                     stringsAsFactors = F)
+  colors[as.numeric(as.factor(colors$lg))%%2==0,"col"]<-pt.cols[2]
+  plot(new.dat$plot.pos,new.dat[,fst.name],
+       xlim=c(x.min,x.max),ylim=y.lim,...,
+       cex=pt.cex, col=colors$col,
+       bty="n",axes=F, xlab="", ylab="")
+  #optionally add the y-axis
+  if(axis.size>0){
+    axis(2, at = seq(round(y.lim[1],2),round(y.lim[2],2),
+                     round((y.lim[2]-y.lim[1])/2, digits=2)),
+         ylim =y.lim, pos=0,
+         labels=seq(round(y.lim[1],2),round(y.lim[2],2),
+                    round((y.lim[2]-y.lim[1])/2, digits=2)),
+         las=1,tck = -0.01, xlab="", ylab="", cex.axis=axis.size)
+  }
+  #optionally add the x-axis
+  if(!is.null(xlabels)){
+    #is this a boolean or a list?
+    if(class(xlabels)=="logical"){
+      xlabels<-as.character(scaffs.to.plot)
+    }
+    #if there are not indices, they should just be the ones that match scaffolds
+    if(is.null(xlab.indices)){
+      xlab.indices<-which(scaffs.to.plot %in% xlabels)
+    }
+    for(i in 1:length(xlabels)){
+      text(x=median(new.dat[new.dat[,chrom.name] %in% scaffs.to.plot[xlab.indices[i]],"plot.pos"]),
+           y=(y.lim[1]-((y.lim[2]-y.lim[1])/20)),
+           labels=xlabels[i],adj=1,xpd=TRUE,cex=as.numeric(axis.size))
+    }
+  }
+  return(new.dat)
+}
+
+#' Plot genome-wide statistics from a data frame with rectangles.
+#' @note This fst.plot function is more efficient but this will work for most cases (might be buggy in some cases)
+#' @param fst.dat The data.frame containing at least three columns: the statistic to be plotted, the chromosome ID, and the BP ID for each locus. Each row is a locus.
+#' @param ci.dat A vector containing two values, upper and lower cutoff values (in that order).
+#' Points above or below the cutoffs will be colored based on sig.col. Default is NULL, which turns this option off.
+#' @param sig.col A vector containing two color values, the first for points above the upper cutoff value and the second for points below the lower cutoff value.
+#' The defaults are red and yellow.
+#' @param pt.col The color of the points. The default is grey7.
+#' @param fst.name The name of the column containing the statistic to be plotted. Default is "Fst".
+#' @param chrom.name The name of the column containing the chromosome information for each locus. Default is "Chrom".
+#' @param bp.name The name of the column containing the basepair information for each locus. Default is "BP".
+#' @param axis.size The value of cex.axis passed to the plotting of the y-axis. Default is 0.5. Set axis.size = 0 to suppress plotting of the y-axis.
+#' @param scaffold.order A vector or list containing the names of the chromosomes/scaffolds in the order in which you would like them to be plotted.
+#' @param groups A vector indicating which chromosomes to plot (generally to exclude those scaffolds not found in this particular set of statistics due to pruning/filters)
+#' @param print.names A TRUE/FALSE value indicating whether chromosome/scaffold IDs should be printed beneath the x-axis.
+#' @param y.lim The limits for the y-axis.
+#' @param group.boundaries A data.frame with first column naming the chromosomes/linkage groups, second column having the last number of loci on the chromosome for indexing purposes.
+#' e.g.:
+#'  Chrom   End
+#'  LG1     3945850
+#'  LG2     435215
+#' This parameter is used to make multiple plots from the same overall dataset have the same widths.
+#' @return xes The fst.dat data frame with new values in BP, scaled to be the sequence in which points are plotted based on their position on the chromosome and the order the chromosomes are plotted in.
+#' @seealso Flanagan & Jones 2017
+#' @export
+fst.plot.rect<-function(fst.dat,ci.dat=NULL, sig.col=c("red","yellow"),pt.col="grey7",
+                        fst.name="Fst", chrom.name="Chrom", bp.name="BP",axis.size=0.5,
+                        scaffold.order=NULL,groups=NULL,print.names=FALSE,y.lim=NULL,
+                        group.boundaries=NULL,pt.cex=0.5,...){
+  
+  if(!is.null(scaffold.order)){
+    scaff.ord<-scaffold.order$component_id
+    lgs<-scaffold.order$object
+  } else{
+    if(!is.null(group.boundaries)){
+      scaff.ord<-unique(group.boundaries[,1])
+      lgs<-scaff.ord
+    }else{
+      scaff.ord<-unique(fst.dat[,chrom.name])
+      lgs<-scaff.ord
+    }
+  }
+  if(!is.null(groups)){
+    lgs<-groups
+    scaff.ord<-groups
+  }
+  fst.dat[,fst.name]<-as.numeric(as.character(fst.dat[,fst.name]))
+  these.scaffs<-scaff.ord[scaff.ord %in% unique(fst.dat[,chrom.name])]
+  all.scaff<-split(fst.dat, factor(fst.dat[,chrom.name]))
+  
+  #keep only the scaffolds you're plotting
+  if(!is.null(groups))
+  {
+    all.scaff<-all.scaff[names(all.scaff) %in% groups]
+  }
+  
+  group.boundaries<-group.boundaries[scaff.ord,]
+  last.max<-0
+  rect.xs<-NULL
+  addition.values<-0
+  xlist<-NULL
+  #set up the rectangles
+  for(i in 1:length(scaff.ord)){
+    if(is.null(group.boundaries)){
+      new.max<-last.max+nrow(all.scaff[[as.character(scaff.ord[i])]])
+    }else{
+      new.max<-as.numeric(group.boundaries[(group.boundaries[1,]) %in% scaff.ord[i],2])+
+        as.numeric(as.character(last.max))
+    }
+    rect.xs<-rbind(rect.xs,c(last.max, new.max))
+    rownames(rect.xs)[i]<-scaff.ord[i]
+    addition.values<-c(addition.values, new.max)
+    last.max<-new.max
+  }
+  rownames(rect.xs)<-scaff.ord
+  for(i in 1:length(all.scaff)){
+    if(is.null(group.boundaries)){
+      #then this is done by relative position only
+      all.scaff[[i]]<-all.scaff[[i]][order(all.scaff[[i]][,bp.name]),]
+      all.scaff[[i]][,bp.name]<-
+        seq(rect.xs[rownames(rect.xs) %in% names(all.scaff)[i],1]+1,
+            rect.xs[rownames(rect.xs) %in% names(all.scaff)[i],1]+nrow(all.scaff[[i]]),1)
+    }else{
+      #then it takes genome space into account
+      
+      all.scaff[[i]]<-
+        all.scaff[[i]][order(all.scaff[[i]][,bp.name]),]
+      all.scaff[[i]][,bp.name]<-
+        as.numeric(rect.xs[rownames(rect.xs) %in% names(all.scaff)[i],1])+
+        as.numeric(as.character(all.scaff[[i]][,bp.name]))
+      
+    }
+  }
+  #change BP to plot
+  x.max<-max(rect.xs,na.rm=T)
+  x.min<-min(rect.xs,na.rm=T)
+  if(is.null(y.lim)){
+    y.max<-max(fst.dat[,fst.name])+0.1*max(fst.dat[,fst.name])
+    y.min<-min(fst.dat[,fst.name])-0.1*min(fst.dat[,fst.name])
+    if(min(fst.dat[,fst.name]) < 0) {
+      y.min<-min(fst.dat[,fst.name]) - 0.1*min(fst.dat[,fst.name])
+    } else {
+      y.min<-0
+    }
+    
+    y.lim<-c(y.min,y.max)
+  }
+  displacement<-y.lim[1]-((y.lim[2]-y.lim[1])/30)
+  plot(c(x.min,x.max),y.lim,xlim=c(x.min,x.max),
+       ylim=y.lim, col=pt.col,...,
+       bty="n",type="n",	axes=F, xlab="", ylab="")
+  for(i in 1:nrow(rect.xs)){
+    if(i%%2 == 0) {
+      rect.color<-"white"
+    } else {
+      rect.color<-"gray75"
+    }
+    rect(rect.xs[i,1],y.lim[1],rect.xs[i,2],y.lim[2],
+         col=rect.color, border=NA)
+    if(print.names==T){
+      text(x=mean(rect.xs[i,]),
+           y=displacement,labels=rownames(rect.xs)[i],
+           adj=1,xpd=T,srt=45)
+    }
+  }
+  for(i in 1:length(all.scaff)){
+    points(all.scaff[[i]][,bp.name],
+           all.scaff[[i]][,fst.name],
+           pch=19, cex=pt.cex,col=pt.col,
+           xlim=c(x.min,x.max),ylim=y.lim)
+    if(!is.null(ci.dat)){
+      temp.sig<-all.scaff[[i]][all.scaff[[i]][,fst.name] >= ci.dat[1],]
+      points(temp.sig[,bp.name], temp.sig[,fst.name],
+             col=sig.col[1], pch=19, cex=0.5)
+      temp.sig<-all.scaff[[i]][all.scaff[[i]][,fst.name] <= ci.dat[2],]
+      points(temp.sig[,bp.name], temp.sig[,fst.name],
+             col=sig.col[2], pch=19, cex=0.5)
+    }
+  }
+  if(axis.size>0){
+    axis(2, at = seq(round(y.lim[1],2),round(y.lim[2],2),
+                     round((y.lim[2]-y.lim[1])/2, digits=2)),
+         ylim =y.lim, pos=0,
+         labels=seq(round(y.lim[1],2),round(y.lim[2],2),
+                    round((y.lim[2]-y.lim[1])/2, digits=2)),
+         las=1,tck = -0.01, xlab="", ylab="", cex.axis=axis.size)
+  }
+  xes<-do.call("rbind",all.scaff)
+  return(xes)
+}
+
+
+#' This plots the output of structure
+#' @param structure.out A data.frame containing the structure output.
+#' @param k The value of K used in Structure
+#' @param pop.order The order in which the populations are plotted.
+#' @param filename The name of the jpeg for the output file.
+#' @param make.file A boolean (TRUE/FALSE) indicating whether the jpeg should be created
+#' @param plot.new A boolean (TRUE/FALSE) indicating whether this is being added to an existing plot
+#' @param colors A list of colors for the structure colors (if not provided, defaults to rainbow palette)
+#' @param xlabel A boolean (TRUE/FALSE) indicating whether x labels should be plotted (default is TRUE)
+#' @param ylabel An optional label for the y-axis.
+#' @param xlabcol An optional vector of colors for x-axis labels
+#' @param lab.cex Size of the labels
+#' @export
+
+plotting.structure<-function(structure.out, k, pop.order,
+                             filename=paste("str.k",k,".jpeg",sep=""),make.file=TRUE,lab.cex=1,
+                             plot.new=TRUE,colors=NULL,xlabel=TRUE,ylabel=NULL,xlabcol=NULL){
+  str.split<-split(structure.out,structure.out[,1])
+  if(is.null(colors)){
+    bar.colors<-rainbow(k,s=0.5)
+  } else {
+    bar.colors<-colors
+  }
+  if(is.null(xlabcol)){
+    xlabcol<-rep("black",length(str.split))
+  }
+  if(make.file==TRUE){
+    jpeg(filename,width=7, height=1.25, units="in", res=300)
+    par(mfrow=c(1,length(str.split)),mar=c(1,0,0,0), oma=c(1,0,0,0),cex=0.5)
+  } else {
+    if(plot.new==TRUE){
+      par(mfrow=c(1,length(str.split)),mar=c(1,0,0,0), oma=c(1,0,0,0),cex=0.5)
+    }
+  }
+  for(i in 1:length(str.split)){
+    pop.index<-pop.order[i]
+    barplot(height=as.matrix(t(str.split[[pop.index]][,-1])),
+            beside=FALSE, space=0,	border=NA, col=bar.colors,
+            xlab="", ylab="", xaxt='n', yaxt='n')#, new=plot.new)
+    if(xlabel==TRUE){
+      mtext(pop.index, 1, line=0.5, cex=lab.cex, outer=F,col=xlabcol[i])}
+    if(!is.null(ylabel)){
+      if(i == 1) { mtext(ylabel,2,cex=lab.cex) }
+    }
+  }
+  if(make.file==TRUE) {dev.off()}
+}
+
+#' Plot covariance matrix from treemix
+#' @param stem The filename basic stem for that run
+#' @param poporder The list of populations in the order to plot them.
+#' @return cp The covariance matrix in the plotting order.
+treemix.cov.plot<-function(stem,poporder,colors=c("blue","yellow","red"),...){
+  if("package:lattice" %in% search() == FALSE || "package:grid" %in% search()==FALSE){
+    stop("ERROR: packages lattice and grid must be loaded")
+  }
+  cov<-read.table(gzfile(paste(stem, ".cov.gz", sep = "")), as.is = T, head = T, quote = "", comment.char = "")
+  #reorder
+  covplot = data.frame(matrix(nrow = nrow(cov), ncol = ncol(cov)))
+  for(i in 1:length(poporder)){
+    for( j in 1:length(poporder)){
+      
+      covplot[i, j] = cov[which(names(cov)==poporder[i]), which(names(cov)==poporder[j])]
+      rownames(covplot)[i]<-poporder[i]
+      colnames(covplot)[j]<-poporder[j]
+    }
+  }
+  cp<-as.matrix(covplot)
+  cp[lower.tri(cp)]<-NA
+  cp[upper.tri(cp)]<-covplot[upper.tri(covplot)]
+  #set up the colors
+  pal<-colorRampPalette(colors)
+  ncol=80
+  cols<-pal(ncol)
+  cpl<-levelplot(cp,col.regions=cols,alpha.regions=0.7,
+                 scales = list(x=list(rot=90),tck = 0),xlab="",ylab="")
+  print(cpl,...)
+  trellis.focus("legend", side="right", clipp.off=TRUE, highlight=FALSE)
+  grid.text("covariance", 0.2, 0, hjust=0.5, vjust=1.2)
+  trellis.unfocus()
+  return(cp)
+}
---
+++ R/gwscaR_popgen.R
@@ -0,0 +1,241 @@
+#Author: Sarah P. Flanagan (spflanagan.phd@gmail.com)
+#Purpose: Calculate useful population genetics statistics 
+#Package: gwscaR
+
+
+#' sliding window pi and rho - across all snps
+#' @note pi = 1-sum((ni choose 2)/(n choose i)); ni is number of alleles i in sample, n = sum(ni)
+#' @note Jones et al. (2012) used 2500bp sliding windows with a step size 500bp<-more than just SNPs, but I'll just focus on SNPs
+#' @note Hohenlohe did a similar thing and weighted pi by all nt sites (not just SNPs) but rho by SNPs only
+#' @param vcf.row A row of a vcf file; use this in conjunction with apply
+#' @return pi The nucleotide diversity at that site
+#' @example all.pi<-apply(vcf,1,pi)
+calc.pi<-function(vcf.row){
+  alleles<-vcf.alleles(vcf.row)
+  af.num<-table(alleles)
+  n<-sum(af.num)
+  pi<-1-sum(choose(af.num,2))/choose(n,2)
+  return(pi)
+}
+
+#' Calculate rho (private alleles)
+#' @note rho=1 if allele in pop j is only found in that pop and at least one ind was genotyped at that site in each pop; rho = 0 otherwise
+calc.rho<-function(vcf.row,pop.list){
+  pop.alleles<-lapply(pop.list,function(pop){
+    pop.vcf<-cbind(vcf.row[1:9],vcf.row[grep(pop,colnames(vcf.row))])
+    alleles<-vcf.alleles(pop.vcf)
+    af.num<-table(alleles)
+    return(names(af.num))
+  })
+  rho<-0
+  pop.counts<-lapply(pop.alleles,length)
+  if(length(pop.counts[pop.counts==0])>0){
+    unique.counts<-table(unlist(pop.alleles[pop.counts==1]))
+    uni<-unique.counts[unique.counts==1]
+    if(length(uni)>1){
+      uni.matches<-grep(names(uni),pop.alleles)
+      if(length(uni.matches)==1){ #it should only match itself
+        rho<-1}
+    }}
+  return(rho)
+}
+
+#' Calculate a sliding average
+sliding.avg<-function(dat,win.start,width){
+  if((win.start+width)>nrow(dat)){
+    win.end<-nrow(dat)
+  }else {
+    win.end<-(win.start+width)
+  }
+  win.dat<-dat[win.start:win.end,]
+  avg.dat<-data.frame(Avg.Pos=mean(dat[win.start:win.end,1]),
+                      Avg.Pi=mean(win.dat[,2]))
+  return(avg.dat)
+}
+
+#' Calculate a sliding window
+sliding.window<-function(vcf,chr,stat="pi",width=250,pop.list=NULL){
+  avg.dat<-lapply(chr,function(chr){
+    chr.vcf<-vcf[vcf[,1] %in% chr,]
+    if(stat=="pi"){
+      dat<-data.frame(Pos=chr.vcf$POS,Pi=unlist(apply(chr.vcf,1,calc.pi))) }
+    if(stat=="rho"){
+      dat<-data.frame(Pos=chr.vcf$POS,Rho=unlist(apply(chr.vcf,1,calc.rho,pop.list=pop.list))) }
+    steps<-seq(1,nrow(chr.vcf),50)
+    if(stat=="pi"){
+      avg.stat<-do.call("rbind",lapply(steps,sliding.avg,dat=dat,width=width)) }
+    if(stat=="rho"){
+      avg.stat<-do.call("rbind",lapply(steps,sliding.avg,dat=dat,width=width)) }
+    avg.stat$Chr<-rep(chr,nrow(avg.stat))
+    head(avg.stat)
+    return(avg.stat)
+  })
+  return(avg.dat)
+}
+
+#' Generate a distance matrix for a SNP using fsts
+#' @param vcf.row A row of a vcf file
+#' @param pop.list A list of populations in the order you want them to appear in the matrix
+#' @return fst.tree A neighbor-joining tree from the ape package
+#' @example fst.trees<-list()
+#' for(vcf.row in 1: nrow(vcf)){
+#'   fst.trees<-c(fst.trees,get.dist(vcf[vcf.row,],pop.list)) #getting an error
+#' }
+#' @export
+get.dist<-function(vcf.row,pop.list){
+  if(("package:ape" %in% search() == FALSE){
+    stop("ERROR: You must load the package ape for get.dist() to run.")
+  }
+  fst.matrix<-matrix(nrow=length(pop.list),ncol=length(pop.list))
+  for(i in 1:(length(pop.list)-1)){
+    for(j in (i+1):length(pop.list)){
+      pop1<-colnames(vcf.row)[grep(pop.list[i],colnames(vcf.row))]
+      pop2<-colnames(vcf.row)[grep(pop.list[j],colnames(vcf.row))]
+      fst<-fst.one.vcf(vcf.row, pop1,pop2,maf=0,cov.thresh = 0)
+      fst.matrix[i,j]<-fst$Fst
+    }
+  }
+  colnames(fst.matrix)<-pop.list
+  rownames(fst.matrix)<-pop.list
+  fst.nj<-ape::njs(as.dist(t(fst.matrix)))
+  fst.tree<-write.tree(fst.nj,digits=0)
+  return(fst.tree)
+}
+
+#' Generate a treemix file from vcf
+#' 
+treemix.from.vcf<-function(vcf,pop.list){
+  tm.df<-matrix(nrow=nrow(vcf),ncol=length(pop.list))
+  for(i in 1:nrow(vcf)){
+    vcf.row<-vcf[i,]
+    #tm.df<-as.matrix(t(apply(vcf,1,function(vcf.row){ #freaking apply was giving me weird results
+    all.alleles<-names(table(vcf.alleles(vcf.row))) 
+    if(length(all.alleles)==2){
+      this.loc<-do.call("cbind",  
+                        lapply(pop.list,function(pop){
+                          pop.vcf.row<-cbind(vcf.row[1:9],vcf.row[grep(pop,colnames(vcf.row))])
+                          pop.alleles<-vcf.alleles(pop.vcf.row)
+                          pop.counts<-table(pop.alleles)
+                          tm.pop<-"0,0"
+                          if(length(pop.counts)==1){
+                            allele<-names(pop.counts)
+                            if(grep(allele,all.alleles)==1){ #maintain order
+                              tm.pop<-paste(pop.counts[[1]],0,sep=",") 
+                            }else{
+                              tm.pop<-paste(0,pop.counts[[1]],sep=",") }
+                          }
+                          if(length(pop.counts)==2){
+                            tm.pop<-paste(pop.counts[[all.alleles[1]]],pop.counts[[all.alleles[2]]],sep=",") 
+                          }
+                          return(tm.pop)
+                        }))
+    }
+    #return(this.loc)
+    tm.df[i,]<-as.vector(this.loc)
+  }#)))
+  colnames(tm.df)<-pop.list
+  return(tm.df)
+}
+
+#' Calculate isolation by distance per locus
+#' @param ped.file A data.frame in ped format
+#' @param dist.mat A distance matrix containing the geeographic distances between populations
+#' @param pop.order A list with the order for the populations
+#' @return results.mantel A data.frame with two columns containing the mantel test results
+#' @export
+fst.ibd.byloc<-function(ped.file,dist.mat,pop.order){
+  results.mantel<-data.frame()
+  for(i in seq(7,ncol(ped.file),2)){
+    res<-ade4::mantel.rtest(
+      as.dist(t(pairwise.fst(ped.file,i,i+1,pop.order))),
+      as.dist(t(dist.mat)), nrepet=9999)
+    results.mantel<-rbind(results.mantel,cbind(res$obs,res$pvalue))
+  }
+  results.mantel<-as.data.frame(results.mantel)
+  colnames(results.mantel)<-c("Obs","P")
+  return(results.mantel)
+}
+
+#' Calculate pairwise Pst between population pairs
+#' @param dat A dataframe with the trait values, first column must be the pop ID
+#' @param pop.order A list of the order of the populations
+#' @return dat.var A data.frame with the pairwise Pst values
+#' @export
+pairwise.pst<-function(dat, pop.order){
+  #first column must be pop id/grouping factor
+  dat.split<-split(dat, factor(dat[,1]))
+  dat.var<-as.data.frame(setNames(
+    replicate(length(pop.order),numeric(0), simplify = F), pop.order))
+  for(i in 1:(length(pop.order)-1)){
+    for(j in (i+1):length(pop.order)){
+      temp.data<-rbind(as.data.frame(dat.split[[pop.order[i]]]),
+                       as.data.frame(dat.split[[pop.order[j]]]))
+      colnames(temp.data)<-c("PopID","Var")
+      temp.data$PopID<-factor(temp.data$PopID)
+      anv <- nlme::lme(fixed=Var ~ 1, random=~1|PopID,data=temp.data)
+      varcomp <- nlme::VarCorr(anv)
+      v.btwn<- as.numeric(varcomp[1])
+      v.wthn <- as.numeric(varcomp[2])
+      pst <- v.btwn/(v.btwn+2*v.wthn)
+      dat.var[pop.order[i],pop.order[j]]<-pst
+      #aov.var<-summary.aov(
+      #	aov(temp.data[,2]~temp.data[,1]))[[1]]$`Sum Sq`
+      #aov.df<-summary.aov(
+      #	aov(temp.data[,2]~temp.data[,1]))[[1]]$`Df`
+      #dat.var[pop.order[i],pop.order[j]]<-aov.var[2]/(aov.var[2]+
+      #	(2*(aov.var[1]/(aov.df[2]-1))))
+    }
+  }
+  dat.var<-rbind(dat.var,rep(NA, ncol(dat.var)))
+  rownames(dat.var)<-colnames(dat.var)
+  return(dat.var)
+}
+
+#' Calulate pairwise Psts and test for IBD
+#' @param trait.df A data frame with trait values for all traits and all individuals
+#' @param comp.df A dataframe with the distance values
+#' @param id.index The index value for the trait being calculated
+#' @param pop.order A vector of population names in the order you want them.
+#' @return results.mantel A data.frame with the results of the mantel test
+#' @export
+pst.mantel<-function(trait.df,comp.df,id.index,pop.order){
+  results.mantel<-data.frame()
+  for(i in 3:ncol(trait.df)){
+    res<-ade4::mantel.rtest(
+      as.dist(t(pairwise.pst(trait.df[,c(id.index,i)],pop.order))),
+      as.dist(t(comp.df)), nrepet=9999)
+    results.mantel<-rbind(results.mantel,cbind(res$obs,res$pvalue))
+  }
+  results.mantel<-as.data.frame(results.mantel)
+  rownames(results.mantel)<-colnames(trait.df)[3:ncol(trait.df)]
+  colnames(results.mantel)<-c("Obs","P")
+  return(results.mantel)
+}
+
+
+#' Compare Pst and Fst locus by locus from a ped file
+#' @param ped.file A data.frame with data in ped format
+#' @param trait.df A data.frame containing all of the traits data
+#' @param pop.order An order in which the populations should be analyzed
+#' @param trait.ind The trait index in the trait.df
+#' @return results.list A dataframe containing the restuls of the mantel test (one column for observations and one for p-values)
+#' @export
+fst.pst.byloc<-function(ped.file,trait.df,pop.order,trait.ind){
+  results.list<-list()
+  for(j in 3:ncol(trait.df)){
+    results.mantel<-data.frame()
+    for(i in seq(7,ncol(ped.file),2)){
+      res<-ade4::mantel.rtest(
+        as.dist(t(pairwise.fst(ped.file,i,i+1,pop.order))),
+        as.dist(t(pairwise.pst(trait.df[,c(trait.ind,j)],pop.order))),
+        nrepet=9999)
+      results.mantel<-rbind(results.mantel,cbind(res$obs,res$pvalue))
+    }
+    results.mantel<-as.data.frame(results.mantel)
+    colnames(results.mantel)<-c("Obs","P")
+    results.list<-append(results.list,data.frame(results.mantel))
+  }
+  #names(results.list)<-colnames(trait.df)[3:ncol(trait.df)]
+  return(results.list)
+}
+
